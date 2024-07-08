ef9de24dab3711fa584dcb388df49af5be733308
Exanubes <blog@exanubes.com>
2024-07-08 18:27:52 +0200

    Create Alias Record

--- index.ts
+++ index.ts
@@ -4,16 +4,31 @@ import { setupWebsocketApi } from "./src/setup-websocket-api";
 
 const config = new pulumi.Config();
 const CERTIFICATE_ARN = config.requireSecret("certificateArn");
+const HOSTED_ZONE_ID = config.requireSecret("hostedZoneId");
+const DOMAIN_NAME = config.require("domainName");
 
 const { api, stage } = setupWebsocketApi();
 
 const domainName = new aws.apigatewayv2.DomainName("domain-name", {
-  domainName: "exanub.es",
+  domainName: DOMAIN_NAME,
   domainNameConfiguration: {
     certificateArn: CERTIFICATE_ARN,
     endpointType: "REGIONAL",
     securityPolicy: "TLS_1_2",
   },
+});
+
+new aws.route53.Record("alias", {
+  name: DOMAIN_NAME,
+  type: aws.route53.RecordType.A,
+  zoneId: HOSTED_ZONE_ID,
+  aliases: [
+    {
+      name: domainName.domainNameConfiguration.targetDomainName,
+      zoneId: domainName.domainNameConfiguration.hostedZoneId,
+      evaluateTargetHealth: true,
+    },
+  ],
 });
 
 export const ws_api_url = pulumi.interpolate`${api.apiEndpoint}/${stage.name}/`;
